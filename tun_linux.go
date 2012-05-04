941c0857e58f0e0457aec6acc4ac9e5f4f40f3df
David Anderson <dave@natulte.net>
2012-05-04 01:10:34 -0700

    Turns out, checkExec does nothing that exec.Command doesn't do already.

--- tun_linux.go
+++ tun_linux.go
@@ -7,18 +7,11 @@ import (
 	"code.google.com/p/tuntap"
 )
 
-func checkExec(args ...string) error {
-	cmd := exec.Command(args[0], args[1:]...)
-	cmd.Stdout = nil
-	cmd.Stderr = nil
-	return cmd.Run()
-}
-
 func setupTun(tun *tuntap.Interface, addr *net.IPNet) error {
-	if err := checkExec("sudo", "ip", "-6", "addr", "add", "dev", tun.Name(), "local", addr.String()); err != nil {
+	if err := exec.Command("sudo", "ip", "-6", "addr", "add", "dev", tun.Name(), "local", addr.String()).Run(); err != nil {
 		return err
 	}
-	if err := checkExec("sudo", "ip", "link", "set", tun.Name(), "mtu", "1280", "up"); err != nil {
+	if err := exec.Command("sudo", "ip", "link", "set", tun.Name(), "mtu", "1280", "up").Run(); err != nil {
 		return err
 	}
 	return nil
